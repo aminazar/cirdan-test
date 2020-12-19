9e08011aaedaef5e6f46e50c185157dd00d67574
Amin Azarbadegan <amin@bentoak.systems>
2020-12-19 23:47:47 +0330

    Basic products list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import PRODUCTS from '../../sample_products.json';
+import moment from 'moment';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  products = [];
+  constructor() {
+    this.products = PRODUCTS.map(p => ({...p, tagsCSV: p.tags.join(', '), formattedDate: moment(p.issue_date).format('DD MMM YYYY')}));
+  }
 
   ngOnInit(): void {
   }
